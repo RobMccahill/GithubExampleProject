7da7a17d3576a26699d686adee66e88601d3a34e
Robert Mccahill <robert.mccahil@gmail.com>
2022-02-12 16:13:21 +0000

    Initial Commit

---
+++ Github Sample ProjectUITests/Github_Sample_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Github_Sample_ProjectUITestsLaunchTests.swift
+//  Github Sample ProjectUITests
+//
+//  Created by Robert Mccahill on 12/02/2022.
+//
+
+import XCTest
+
+class Github_Sample_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
